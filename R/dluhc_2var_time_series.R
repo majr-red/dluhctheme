16153a3ce80d374cfce5cbc99cc116802bdf6dab
Niall McSharry <niall.mcsharry@levellingup.gov.uk>
2023-01-16 14:52:22 +0000

    added in roxygen skeleten

--- R/dluhc_2var_time_series.R
+++ R/dluhc_2var_time_series.R
@@ -1,3 +1,14 @@
+#' Title
+#'
+#' @param .data A dataframe with the data in a long format with one column for the date (in the format "%Y-%m-%d","%d/%m/%Y","%d/%m/%y","%e/%m/%Y","%d-%b-%Y" or "%d-%b-%y") and one column for the variable value
+#' @param xcol The column name of the dataframe which contains the date variable
+#' @param ycol The column name of the dataframe which contains the value
+#' @param groupcol The column name which contains the grouping variable (such as country, region or type)
+#'
+#' @return A ggplot2 object
+#' @export
+#'
+#' @examples
 dluhc_2var_time_series <- function(.data,xcol,ycol,groupcol){
   library(tidyverse)
   .data <- .data %>%

8e812b0a4a353153df676d895434c8336d6c0bb2
Niall McSharry <niall.mcsharry@levellingup.gov.uk>
2023-01-16 09:56:31 +0000

    initial commit, copied from housing stats team

---
+++ R/dluhc_facet_bar_chart.R
@@ -0,0 +1,10 @@
+dluhc_facet_bar_chart <- function(.data,xcol,ycol,groupcol,textsize=1){
+.data <- mutate(.data,variable = {{groupcol}})
+
+ggplot(data = .data,aes(x={{xcol}},y={{ycol}},fill=variable)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~factor(variable), scales='fixed',strip.position="top") +
+    dluhc_theme(size = textsize) +
+  theme(legend.position = "none")
+}
+

0728cc786df54c76d8fe481ced6f7cc275d00ddc
Niall McSharry <niall.mcsharry@levellingup.gov.uk>
2023-01-16 15:51:57 +0000

    updated Rd file

--- R/dluhc_2var_time_series.R
+++ R/dluhc_2var_time_series.R
@@ -1,6 +1,6 @@
 #' Creating a 2 line time series graph in a DLUHC style
 #'
-#' @param .data A dataframe with the data in a long format with one column for the date (in the format "%Y-%m-%d","%d/%m/%Y","%d/%m/%y","%e/%m/%Y","%d-%b-%Y" or "%d-%b-%y") and one column for the variable value
+#' @param .data A dataframe with the data in a long format with one column for the date and one column for the variable value
 #' @param xcol The column name of the dataframe which contains the date variable
 #' @param ycol The column name of the dataframe which contains the value
 #' @param groupcol The column name which contains the grouping variable (such as country, region or type)

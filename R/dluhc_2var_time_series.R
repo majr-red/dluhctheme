b0ff97b381ec7f284b85e6d707ebbfde198be22f
Niall McSharry <niall.mcsharry@levellingup.gov.uk>
2023-01-18 10:54:48 +0000

    Renamed the date column, added and example and created an error check for the date column

--- R/dluhc_2var_time_series.R
+++ R/dluhc_2var_time_series.R
@@ -1,7 +1,7 @@
 #' Create a 2 line time series graph in a DLUHC style
 #'
 #' @param .data A dataframe with the data in a long format with one column for the date and one column for the variable value
-#' @param xcol The column name of the dataframe which contains the date variable
+#' @param datecol The column name of the dataframe which contains the date variable
 #' @param ycol The column name of the dataframe which contains the value
 #' @param groupcol The column name which contains the grouping variable (such as country, region or type)
 #'
@@ -9,16 +9,28 @@
 #' @export
 #'
 #' @examples
-dluhc_2var_time_series <- function(.data,xcol,ycol,groupcol){
+#' df <- dplyr::filter(gapminder::gapminder,country=="Australia"|country=="China")
+#' dluhc_2var_time_series(.data=df,datecol = year, ycol = lifeExp, groupcol = country, dateformat = "%Y")
+
+dluhc_2var_time_series <- function(.data,datecol,ycol,groupcol,dateformat = "%Y-%m-%d"){
   library(tidyverse)
+
+  is.convertible.to.date <- function(x) !is.na(as.Date(as.character(x), tz = 'UTC', format = dateformat))
+
+  if(any(is.convertible.to.date(pull(.data,{{datecol}}))==FALSE)){
+    stop("Date column not in format specified. Check the dateformat argument in the function")
+    }
+
+
+
   .data <- .data %>%
-    mutate(Date = as.Date({{xcol}},tryFormats = c("%Y-%m-%d","%d/%m/%Y","%d/%m/%y","%e/%m/%Y","%d-%b-%Y","%d-%b-%y"))) %>%
+    mutate(Date = as.Date(as.character({{datecol}}),tryFormats = dateformat)) %>%
     mutate(value = {{ycol}}) %>%
     mutate(variable = {{groupcol}})
 
   .data %>%
     ggplot(aes(x = Date,y = value)) +
-    geom_line(size = 1.5, aes(color = variable)) +
+    geom_line(size = 1, aes(color = variable)) +
     scale_color_manual(values=c("#012169","#6b98fe")) +
     theme_classic(base_size = 18) +
     scale_y_continuous(expand = c(0,0)) +

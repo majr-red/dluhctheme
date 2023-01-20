f285853c542ed57dfa8caab06d06ae80f314eb4f
Niall McSharry <niall.mcsharry@levellingup.gov.uk>
2023-01-20 09:37:12 +0000

    rename function

--- R/facet_barchart.R
+++ R/facet_barchart.R
@@ -1,10 +1,11 @@
-dluhc_facet_bar_chart <- function(.data,xcol,ycol,groupcol,textsize=1){
+facet_barchart <- function(.data,xcol,ycol,groupcol,textsize=1){
 .data <- mutate(.data,variable = {{groupcol}})
 
 ggplot(data = .data,aes(x={{xcol}},y={{ycol}},fill=variable)) +
   geom_bar(stat="identity") +
   facet_wrap(~factor(variable), scales='fixed',strip.position="top") +
     dluhc_theme(size = textsize) +
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  scale_fill_manual = c("#012169","blue","orange","purple","black","red","green")
 }
 
